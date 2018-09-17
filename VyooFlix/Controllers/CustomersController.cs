2f0fc1a7a2498c23a130b5da447c525b7d4e1407
epikkoder <ronaldsahagun87@gmail.com>
2018-09-16 19:52:13 -0700

    Used ApplicationDbContext to grab Customers data from the database table

--- VyooFlix/Controllers/CustomersController.cs
+++ VyooFlix/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using VyooFlix.Models;
 
@@ -6,26 +7,29 @@ namespace VyooFlix.Controllers
 {
     public class CustomersController : Controller
     {
-	    private List<Customer> customers = new List<Customer>
-	    {
-			new Customer {Id = 1, Name = "John Smith"},
-			new Customer {Id = 2, Name = "Amy Lee"}
-	    };
+	    private ApplicationDbContext _context;
 
-        // GET: Customers
+	    public CustomersController()
+	    {
+		    _context = new ApplicationDbContext();
+	    }
+
+	    protected override void Dispose(bool disposing)
+	    {
+		    _context.Dispose();
+	    }
+
+	    // GET: Customers
         public ActionResult Index()
         {
+	        var customers = _context.Customers.ToList();
+
             return View(customers);
         }
 
 	    public ActionResult Details(int id)
 	    {
-		    Customer customer = null;
-		    foreach (Customer c in customers)
-		    {
-			    if (c.Id == id)
-				    customer = c;
-		    }
+		    var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
 		    if (customer == null)
 			    return HttpNotFound();
