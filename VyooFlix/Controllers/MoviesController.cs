4954b8918307b3c7e6752fb9e459c17b65f50504
epikkoder <ronaldsahagun87@gmail.com>
2019-01-18 09:50:43 -0800

    Save changes

--- VyooFlix/Controllers/MoviesController.cs
+++ VyooFlix/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,15 +11,23 @@ namespace VyooFlix.Controllers
 {
     public class MoviesController : Controller
     {
-	    List<Movie> movies = new List<Movie>
+	    private ApplicationDbContext _context;
+
+	    public MoviesController()
 	    {
-		    new Movie {Id = 1, Name = "Shrek"},
-		    new Movie {Id = 2, Name = "Wall-E"}
-	    };
+		    _context = new ApplicationDbContext();
+	    }
+
+	    protected override void Dispose(bool disposing)
+	    {
+			_context.Dispose();
+	    }
 
 		// GET: Movies/Index
 		public ActionResult Index()
-        {
+		{
+			var movies = _context.Movies.Include(m => m.Genre).ToList();
+
 			return View(movies);
 		}
 
@@ -28,19 +37,19 @@ namespace VyooFlix.Controllers
 		//    return Content(year + "/" + month);
 		//   }
 
-	    public ActionResult Details(int id)
-	    {
-		    Movie movie = null;
-		    foreach (Movie m in movies)
-		    {
-			    if (m.Id == id)
-				    movie = m;
-		    }
+	    //public ActionResult Details(int id)
+	    //{
+		   // Movie movie = null;
+		   // foreach (Movie m in movies)
+		   // {
+			  //  if (m.Id == id)
+				 //   movie = m;
+		   // }
 
-		    if (movie == null)
-			    return HttpNotFound();
+		   // if (movie == null)
+			  //  return HttpNotFound();
 
-		    return View(movie);
-	    }
+		   // return View(movie);
+	    //}
 	}
 }
