daec18671a8d4fde821bad4d4ca615279ed15534
epikkoder <ronaldsahagun87@gmail.com>
2019-09-09 09:34:00 -0700

    Added AutoMapper

---
+++ VyooFlix/App_Start/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VyooFlix.Dtos;
+using VyooFlix.Models;
+
+namespace VyooFlix.App_Start
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			var config = new MapperConfiguration(cfg =>
+			{
+				cfg.CreateMap<Customer, CustomerDto>();
+				cfg.CreateMap<CustomerDto, Customer>();
+			});
+		}
+	}
+}
