ba5c83be920f8dc81ac71c21b5d0fc041ab9b3e4
epikkoder <ronaldsahagun87@gmail.com>
2019-09-22 15:02:07 -0700

    Changed return values for GetCustomer and CreateCustomer

--- VyooFlix/Controllers/Api/CustomersController.cs
+++ VyooFlix/Controllers/Api/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,80 +10,80 @@ using VyooFlix.Models;
 
 namespace VyooFlix.Controllers.Api
 {
-	public class CustomersController : ApiController
-	{
-		private ApplicationDbContext _context;
-		protected readonly IMapper _mapper;
-		private readonly MapperConfiguration config;
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+        protected readonly IMapper _mapper;
+        private readonly MapperConfiguration config;
 
-		public CustomersController()
-		{
-			_context = new ApplicationDbContext();
-			config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
-			_mapper = config.CreateMapper();
-		}
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+            config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+            _mapper = config.CreateMapper();
+        }
 
-		// GET /api/customers
-		public IEnumerable<CustomerDto> GetCustomers()
-		{
-			return _context.Customers.ToList().Select(_mapper.Map<Customer, CustomerDto>);
-		}
+        // GET /api/customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            return _context.Customers.ToList().Select(_mapper.Map<Customer, CustomerDto>);
+        }
 
-		// GET /api/customers/1
-		public CustomerDto GetCustomer(int id)
-		{
-			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+        // GET /api/customers/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-			if (customer == null)
-				throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (customer == null)
+                return NotFound();
 
-			return _mapper.Map<Customer, CustomerDto>(customer);
-		}
+            return Ok(_mapper.Map<Customer, CustomerDto>(customer));
+        }
 
-		// POST /api/customers
-		[HttpPost]
-		public CustomerDto CreateCustomer(CustomerDto customerDto)
-		{
-			if (!ModelState.IsValid)
-				throw new HttpResponseException(HttpStatusCode.BadRequest);
+        // POST /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-			var customer = _mapper.Map<CustomerDto, Customer>(customerDto);
-			_context.Customers.Add(customer);
-			_context.SaveChanges();
+            var customer = _mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
 
-			customerDto.Id = customer.Id;
+            customerDto.Id = customer.Id;
 
-			return customerDto;
-		}
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
+        }
 
-		// PUT /api/customers/1
-		[HttpPut]
-		public void UpdateCustomer(int id, CustomerDto customerDto)
-		{
-			if (!ModelState.IsValid)
-				throw new HttpResponseException(HttpStatusCode.BadRequest);
+        // PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
 
-			var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-			if (customerInDb == null)
-				throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
 
-			_mapper.Map(customerDto, customerInDb);
+            _mapper.Map(customerDto, customerInDb);
 
-			_context.SaveChanges();
-		}
+            _context.SaveChanges();
+        }
 
-		// DELETE /api/customers/1
-		[HttpDelete]
-		public void DeleteCustomer(int id)
-		{
-			var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-			if (customerInDb == null)
-				throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
 
-			_context.Customers.Remove(customerInDb);
-			_context.SaveChanges();
-		}
-	}
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+    }
 }
--- VyooFlix/Dtos/CustomerDto.cs
+++ VyooFlix/Dtos/CustomerDto.cs
@@ -16,7 +16,7 @@ namespace VyooFlix.Dtos
 
 		public byte MembershipTypeId { get; set; }
 
-		[Min18YearsIfAMember]
+		//[Min18YearsIfAMember]
 		public DateTime? BirthDate { get; set; }
 	}
 }
