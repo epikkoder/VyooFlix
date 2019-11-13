83324d789594cf6278b6837a23736941b0895f52
epikkoder <ronaldsahagun87@gmail.com>
2019-11-13 09:44:57 -0800

    Replaced native JS confirm dialog with Bootbox confirm on Customer delete

--- VyooFlix/App_Start/BundleConfig.cs
+++ VyooFlix/App_Start/BundleConfig.cs
@@ -19,7 +19,8 @@ namespace VyooFlix
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
+					  "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap-superhero.css",
