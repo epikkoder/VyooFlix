373e53f1f0cf6bbda6ba28c96876bda2a3010509
epikkoder <ronaldsahagun87@gmail.com>
2019-11-17 10:01:26 -0800

    Added DataTables

--- VyooFlix/App_Start/BundleConfig.cs
+++ VyooFlix/App_Start/BundleConfig.cs
@@ -2,29 +2,30 @@
 
 namespace VyooFlix
 {
-	public class BundleConfig
-	{
-		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-		public static void RegisterBundles(BundleCollection bundles)
-		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.js",
+                        "~/Scripts/DataTables/js/jquery.dataTables.min.js",
+                        "~/Scripts/DataTables/js/dataTables.bootstrap.min.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
 
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.js"));
-
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap-superhero.css",
-					  "~/Content/site.css"));
-		}
-	}
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap-superhero.css",
+                      "~/Content/DataTables/dataTables.bootstrap.min.css",
+                      "~/Content/site.css"));
+        }
+    }
 }
