aca78f282a81e3c32e92c88d2d9a6014fe392ebe
epikkoder <ronaldsahagun87@gmail.com>
2019-11-18 09:16:02 -0800

    Used DataTables to call the API to get the customers

--- VyooFlix/Controllers/CustomersController.cs
+++ VyooFlix/Controllers/CustomersController.cs
@@ -68,9 +68,7 @@ namespace VyooFlix.Controllers
 		// GET: Customers
 		public ActionResult Index()
 		{
-			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-			return View(customers);
+			return View();
 		}
 
 		public ActionResult Details(int id)
